442d5a159c6a8ed0d84e460263e5a06bdca65570
Szymon287099 <287099@student.pwr.edu.pl>
2025-03-17 16:34:29 +0100

    Add files via upload

---
+++ 287099/z1/z1.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+void z1a(int n) {
+    int x = 0;
+    for (int i = 0; i < n; i++) {
+        std::cout << x;
+        x += 1;
+        if (x == 10) {
+            x = 0;
+        }
+    }
+}
+
+void z1b(int n) {
+    int x = 0;
+    for (int i = 0; i < n; i++) {
+        std::cout << x;
+        x += 1;
+        if (x == 2) {
+            x = 0;
+        }
+    }
+}
+
+void z1c(int n) {
+    int x = 0;
+    for (int i = 0; i < n; i++) {
+        std::cout << x;
+        x += 1;
+        if (x == 4) {
+            x = 0;
+        }
+    }
+}
+
+void z1f(int n) {
+    int x = 3;
+    for (int i = 0; i < n; i++) {
+        if (x == 3) {
+            std::cout << x;
+            x = 7;
+        } else {
+            std::cout << x;
+            x = 3;
+        }
+    }
+}
+
+void z2e(int n) {
+    for (int i = 1; i <= n; i++) {
+        std::cout << i;
+        for (int j = i + 1; j <= n; j++) {
+            std::cout << j;
+        }
+        std::cout << "\n";
+    }
+}
+
+void z2g(int n) {
+    for (int i = n; i > 0; i--) {
+        std::cout << i;
+        for (int j = i - 1; j > 0; j--) {
+            std::cout << j;
+        }
+        std::cout << "\n";
+    }
+}
+
+int main()
+{
+    z2e(5);
+}
---
+++ 287099/z2/z2/z2.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+
+double mem;
+bool mem_used;
+double add(double a, double b) {
+    return a + b;
+    mem = a + b;
+    mem_used = true;
+}
+double sub(double a, double b) {
+    return a - b;
+    mem = a - b;
+    mem_used = true;
+}
+double mul(double a, double b) {
+    return a * b;
+    mem = a * b;
+    mem_used = true;
+}
+double divi(double a, double b) {
+    if (b == 0) {
+        std::cout << "Nie mozna dzielic przez 0!" << std::endl;
+        return 0;
+    }
+    else {
+        return a / b;
+        mem = a / b;
+        mem_used = true;
+    }
+}
+double mod(double a, double b) {
+    if (b == 0) {
+        std::cout << "Nie mozna dzielic przez 0!" << std::endl;
+        return 0;
+    }
+    else {
+        return a - (int)(a / b) * b;
+        mem = a - (int)(a / b) * b;
+        mem_used = true;
+    }
+}
+void erase() {
+    mem = 0;
+    mem_used = false;
+}
+void menu() {
+    double a;
+    double b;
+    int dzialanie;
+    std::cout << "Podaj dwie liczby oraz dzialanie (1 - '+', 2 - '-', 3 - '*', 4 - '/', 5 - '%', 6 - 'WYCZYSC M)";
+    std::cin >> a >> b >> dzialanie;
+    switch (dzialanie) {
+    case 1:
+        add(a, b);
+        break;
+    case 2:
+        sub(a, b);
+        break;
+    case 3:
+        mul(a, b);
+        break;
+    case 4:
+        divi(a, b);
+        break;
+    case 5:
+        mod(a, b);
+        break;
+    }
+}
+
+int main() {
+    menu();
+    return 0;
+}
