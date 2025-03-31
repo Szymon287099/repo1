dd1a718d871eacb318120ae6f0ba2fe03786772a
Szymon287099 <287099@student.pwr.edu.pl>
2025-03-31 13:40:56 +0200

    Update z2.cpp

--- 287099/z2/z2/z2.cpp
+++ 287099/z2/z2/z2.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
 
 class Kalkulator {
-public:
+private:
     double mem;
     bool mem_used;
 
+public:
     Kalkulator() : mem(0), mem_used(false) {}
 
     double add(double a, double b) {
-        mem = a + b;
-        mem_used = true;
+        setMem(a + b);
         return mem;
     }
 
     double sub(double a, double b) {
-        mem = a - b;
-        mem_used = true;
+        setMem(a - b);
         return mem;
     }
 
     double mul(double a, double b) {
-        mem = a * b;
-        mem_used = true;
+        setMem(a * b);
         return mem;
     }
 
@@ -30,11 +28,8 @@ public:
             std::cout << "Nie mozna dzielic przez 0!" << std::endl;
             return 0;
         }
-        else {
-            mem = a / b;
-            mem_used = true;
-            return mem;
-        }
+        setMem(a / b);
+        return mem;
     }
 
     double mod(double a, double b) {
@@ -42,11 +37,8 @@ public:
             std::cout << "Nie mozna dzielic przez 0!" << std::endl;
             return 0;
         }
-        else {
-            mem = a - (int)(a / b) * b;
-            mem_used = true;
-            return mem;
-        }
+        setMem(a - (int)(a / b) * b);
+        return mem;
     }
 
     void erase() {
@@ -54,40 +46,76 @@ public:
         mem_used = false;
     }
 
+    double getMem() const {
+        return mem;
+    }
+
+    void setMem(double value) {
+        mem = value;
+        mem_used = true;
+    }
+
     void menu() {
         double a, b;
         int dzialanie;
-        std::cout << "Podaj dwie liczby oraz dzialanie (1 - '+', 2 - '-', 3 - '*', 4 - '/', 5 - '%', 6 - 'WYCZYSC M'): ";
-        std::cin >> a >> b >> dzialanie;
-        switch (dzialanie) {
-        case 1:
-            std::cout << add(a, b) << std::endl;
-            break;
-        case 2:
-            std::cout << sub(a, b) << std::endl;
-            break;
-        case 3:
-            std::cout << mul(a, b) << std::endl;
-            break;
-        case 4:
-            std::cout << divi(a, b) << std::endl;
-            break;
-        case 5:
-            std::cout << mod(a, b) << std::endl;
-            break;
-        case 6:
-            erase();
-            std::cout << "Pamiec wyczyszczona" << std::endl;
-            break;
-        default:
-            std::cout << "Nieprawidlowe dzialanie!" << std::endl;
-            break;
+        while (true) {
+            std::cout << "Podaj dwie liczby oraz dzialanie (1 - '+', 2 - '-', 3 - '*', 4 - '/', 5 - '%', 6 - 'WYCZYSC M', 0 - WYJSCIE): ";
+            std::cin >> a >> b >> dzialanie;
+            if (dzialanie == 0) {
+                std::cout << "Koniec programu." << std::endl;
+                break;
+            }
+            switch (dzialanie) {
+            case 1:
+                std::cout << add(a, b) << std::endl;
+                break;
+            case 2:
+                std::cout << sub(a, b) << std::endl;
+                break;
+            case 3:
+                std::cout << mul(a, b) << std::endl;
+                break;
+            case 4:
+                std::cout << divi(a, b) << std::endl;
+                break;
+            case 5:
+                std::cout << mod(a, b) << std::endl;
+                break;
+            case 6:
+                erase();
+                std::cout << "Pamiec wyczyszczona" << std::endl;
+                break;
+            default:
+                std::cout << "Nieprawidlowe dzialanie!" << std::endl;
+                break;
+            }
         }
     }
 };
 
 int main() {
     Kalkulator k;
+
+    std::cout << "Testowanie kalkulatora:\n";
+    std::cout << "5 + 3 = " << k.add(5, 3) << "\n";
+    std::cout << "Stan pamieci: " << k.getMem() << "\n";
+
+    std::cout << "10 - 2 = " << k.sub(10, 2) << "\n";
+    std::cout << "Stan pamieci: " << k.getMem() << "\n";
+
+    std::cout << "4 * 7 = " << k.mul(4, 7) << "\n";
+    std::cout << "Stan pamieci: " << k.getMem() << "\n";
+
+    std::cout << "20 / 4 = " << k.divi(20, 4) << "\n";
+    std::cout << "Stan pamieci: " << k.getMem() << "\n";
+
+    std::cout << "10 % 3 = " << k.mod(10, 3) << "\n";
+    std::cout << "Stan pamieci: " << k.getMem() << "\n";
+
+    k.erase();
+    std::cout << "Pamiec po wyczyszczeniu: " << k.getMem() << "\n";
+    
     k.menu();
+
     return 0;
 }
